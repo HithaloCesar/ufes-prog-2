93e4251b8207da7fd9c021007b9b8d3ef5b2d7b4
Hithalo <hithalo@hithalo.com>
2025-11-07 13:15:44 -0300

    fix(05-pointers/13): redo solution without globals

--- 05-pointers/13-scrolling-panel/Respostas/Hithalo/main.c
+++ 05-pointers/13-scrolling-panel/Respostas/Hithalo/main.c
@@ -5,30 +5,37 @@
 
 #define TAMANHO_DISPLAY 30
 
-// Unfortunately, static global variables are needed, since the header can't be
-// changed
+typedef struct {
+    char msg[NUM_MAX_MSGS][TAM_MAX_MSG];
+    int numMsgs;
+} Mensagens;
 
-static int g_numMsgs;
-static char g_msgs[NUM_MAX_MSGS][TAM_MAX_MSG];
+void obtemMensagens(
+    char msg[NUM_MAX_MSGS][TAM_MAX_MSG],
+    int *numMsgs,
+    void *dadosUsuario
+) {
+    Mensagens *msgsOriginais = (Mensagens*)dadosUsuario;
 
-void obtemMensagens(char msg[NUM_MAX_MSGS][TAM_MAX_MSG], int *numMsgs) {
-    *numMsgs = g_numMsgs;
+    *numMsgs = msgsOriginais->numMsgs;
     for (int i = 0; i < *numMsgs; i++) {
-        strcpy(msg[i], g_msgs[i]);
+        strcpy(msg[i], msgsOriginais->msg[i]);
     }
 }
 
 int main(void) {
-    scanf("%d", &g_numMsgs);
+    Mensagens msgs;
 
-    for (int i = 0; i < g_numMsgs; i++) {
-        scanf(" %[^\n]", g_msgs[i]);
+    scanf("%d", &msgs.numMsgs);
+
+    for (int i = 0; i < msgs.numMsgs; i++) {
+        scanf(" %[^\n]", msgs.msg[i]);
     }
 
     int tempoFim;
     scanf("%d", &tempoFim);
 
-    RolaMsg(obtemMensagens, TAMANHO_DISPLAY, tempoFim);
+    RolaMsg(obtemMensagens, TAMANHO_DISPLAY, tempoFim, &msgs);
 
     return EXIT_SUCCESS;
 }
--- 05-pointers/13-scrolling-panel/Respostas/Hithalo/rolagem.c
+++ 05-pointers/13-scrolling-panel/Respostas/Hithalo/rolagem.c
@@ -1,23 +1,32 @@
-#define _DEFAULT_SOURCE
+#define _DEFAULT_SOURCE // For usleep (deprecated, but enough for this)
 
 #include "rolagem.h"
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 
-void RolaMsg(FptrMsg FuncMsg, int tamanhoDisplay, int tempoFim) {
-    char mensagens[NUM_MAX_MSGS][TAM_MAX_MSG];
+void RolaMsg(
+    FptrMsg FuncMsg,
+    int tamanhoDisplay,
+    int tempoFim,
+    void *dadosUsuario
+) {
+    char msgs[NUM_MAX_MSGS][TAM_MAX_MSG];
+    int numMsgs;
 
-    int numMsgs = 0;
-    FuncMsg(mensagens, &numMsgs);
+    FuncMsg(msgs, &numMsgs, dadosUsuario);
 
     char mensagensConcatenadas[NUM_MAX_MSGS * TAM_MAX_MSG] = "";
     for (int i = 0; i < numMsgs; i++) {
-        strcat(mensagensConcatenadas, mensagens[i]);
+        strcat(mensagensConcatenadas, msgs[i]);
         strcat(mensagensConcatenadas, " ");
     }
 
     int len = strlen(mensagensConcatenadas);
+
+    printf("\033[H\033[J"); // Clear terminal. Before start, then each "frame".
+    // The statement says it must be printed in the output file, even if it
+    // doesn't make sense.
 
     for (int i = 0; i < tempoFim; i++) {
         for (int j = 0 + i; j < tamanhoDisplay + i; j++) {
@@ -25,10 +34,10 @@ void RolaMsg(FptrMsg FuncMsg, int tamanhoDisplay, int tempoFim) {
         }
         putchar('\n');
 
-        printf("\033[H\033[J"); // Clear terminal
-
-        if (isatty(STDOUT_FILENO)) {
-            usleep(100000); // 0.1 s
+        if (isatty(STDOUT_FILENO)) { // Avoid timeout in autograder.sh
+            usleep(100000); // 0.1 s.
         }
+
+        printf("\033[H\033[J");
     }
 }
