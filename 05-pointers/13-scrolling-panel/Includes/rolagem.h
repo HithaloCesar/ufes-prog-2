554f42189a978b869f5323ac73303b5c20ed9a74
Hithalo <hithalo@hithalo.com>
2025-11-07 12:31:43 -0300

    fix(05-pointers/13): remove need for static global

--- 05-pointers/13-scrolling-panel/Includes/rolagem.h
+++ 05-pointers/13-scrolling-panel/Includes/rolagem.h
@@ -1,24 +1,36 @@
-#ifndef _ROLAGEM_H
-#define _ROLAGEM_H
+#ifndef ROLAGEM_H
+#define ROLAGEM_H
 
 #define TAM_MAX_MSG 1000
 #define NUM_MAX_MSGS 10
 
 /**
- * @brief Ponteiro para função que recebe um array de mensagens e o número de mensagens para rolagem.
+ * @brief Ponteiro para função (callback)
  *
- * @param msg Array de mensagens.
- * @param numMsgs Número de mensagens.
+ * @param msg Array de mensagens (parâmetro de saída).
+ * @param numMsgs Número de mensagens (parâmetro de saída).
+ * @param dadosUsuario Ponteiro "genérico" para os dados do usuário.
  */
-typedef void (*FptrMsg)(char msg[NUM_MAX_MSGS][TAM_MAX_MSG], int * numMsgs);
+typedef void (*FptrMsg)(
+    char msg[NUM_MAX_MSGS][TAM_MAX_MSG],
+    int *numMsgs,
+    void *dadosUsuario
+);
 
 /**
- * @brief Dispara a função de rolagem de mensagens.
+ * @brief Dispara a função de rolagem de mensagens
  *
- * @param FuncMsg Ponteiro para a função que recebe um array de mensagens e o número de mensagens.
+ * @param FuncMsg Ponteiro para a função de callback.
  * @param tamanhoDisplay Tamanho do display.
- * @param tempoFim Tempo de duração da rolagem, que diz respeito a quantidade de deslocamento no painel.
+ * @param tempoFim Tempo de duração da rolagem.
+ * @param dadosUsuario Um ponteiro fornecido pelo usuário (main) que será
+ *                     repassado para o callback 'FuncMsg'.
  */
-void RolaMsg(FptrMsg FuncMsg, int tamanhoDisplay, int tempoFim);
+void RolaMsg(
+    FptrMsg FuncMsg,
+    int tamanhoDisplay,
+    int tempoFim,
+    void *dadosUsuario
+);
 
 #endif
