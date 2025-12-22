40b5d806624f94aa5f1a7004ed870d8a0a19b985
Hithalo <hithalo@hithalo.com>
2025-12-21 23:25:23 -0300

    feat(08-opaque-types/09-business): add solution

---
+++ 08-opaque-types/09-business/Respostas/Hithalo/vendedor.c
@@ -0,0 +1,58 @@
+#include "vendedor.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+struct Vendedor {
+    char* nome;
+    float valor_vendido;
+    float salario;
+    float prct_comissao;
+};
+
+tVendedor *RegistraVendedor(char *nome, float salario, float prct_comissao) {
+    tVendedor *v = calloc(1, sizeof(*v));
+    v->nome = malloc((strlen(nome) + 1) * sizeof(*v->nome));
+
+    strcpy(v->nome, nome);
+    v->salario = salario;
+    v->prct_comissao = prct_comissao;
+
+    return v;
+}
+
+void ApagaVendedor(tVendedor *vendedor) {
+    free(vendedor->nome);
+    free(vendedor);
+}
+
+int VerificaNomeVendedor(tVendedor *vendedor, char *nome) {
+    return !strcmp(vendedor->nome, nome);
+}
+
+void ContabilizaVenda(tVendedor *vendedor, float valor) {
+    vendedor->valor_vendido += valor;
+}
+
+float GetSalario(tVendedor *vendedor) {
+    return vendedor->salario;
+}
+
+float GetComissao(tVendedor *vendedor) {
+    return vendedor->prct_comissao * vendedor->valor_vendido;
+}
+
+float GetTotalVendido(tVendedor *vendedor) {
+    return vendedor->valor_vendido;
+}
+
+float GetTotalRecebido(tVendedor *vendedor) {
+    return GetSalario(vendedor) + GetComissao(vendedor);
+}
+
+void ImprimeRelatorioVendedor(tVendedor *vendedor) {
+    float totalVendido = GetTotalVendido(vendedor);
+    float totalRecebido = GetTotalRecebido(vendedor);
+    printf("\t%s > Total vendido: R$%.2f\n", vendedor->nome, totalVendido);
+    printf("\t\tTotal recebido: R$%.2f\n", totalRecebido);
+}
